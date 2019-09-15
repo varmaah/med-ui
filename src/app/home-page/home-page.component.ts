2cb4eb0c82a9c5571808db6d291400fbcacde7a1
phaneendra varma <phani5797@gmail.com>
2019-09-14 23:01:50 -0400

    added programs in the home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -6,6 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+ //TODO: Move to appropriate service
+  programs = [{
+   description :'etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit ',
+    title : 'Adolescence to  Menopause'
+  },
+  {
+   description :'etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit ',
+    title : 'Brest Health Program'
+  },
+  {
+   description :'etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit ',
+    title : 'Integrated  Digestive, Liver'
+  },
+  {
+   description :'etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit ',
+    title : 'Kidney Transplantation'
+  },
+  {
+   description :'etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit ',
+    title : 'Liver Transplantation Surgery'
+  },
+  {
+   description :'etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit ',
+    title : 'Total Knee Replacement'
+  },
+  {
+   description :'etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit ',
+    title : 'Total Hip Replacement'
+
+  },
+  {
+   description :'etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit ',
+    title : 'Heart Screening Surgery'
+  }];
 
   constructor() { }
 
