0cf4a1cac395db66522beec7125b5eaeb45ab492
phaneendra varma <phani5797@gmail.com>
2019-09-14 23:41:20 -0400

    added scroll to top functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScrollToTopComponent } from './scroll-to-top/scroll-to-top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScrollToTopComponent
   ],
   imports: [
     BrowserModule,
