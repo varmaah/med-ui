ae49d562a9de52926800d95af9ca98f49a4f9b10
phaneendra varma <phani5797@gmail.com>
2019-09-15 18:33:07 -0400

    added why best component

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -2,12 +2,13 @@ import { HomePageRoutes } from './home-page.routing';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
+import { WhyBestComponent } from './why-best/why-best.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HomePageRoutes
   ],
-  declarations: [HomePageComponent]
+  declarations: [HomePageComponent, WhyBestComponent]
 })
 export class HomePageModule { }
---
+++ src/app/home-page/why-best/why-best.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-why-best',
+  templateUrl: './why-best.component.html',
+  styleUrls: ['./why-best.component.css']
+})
+export class WhyBestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
