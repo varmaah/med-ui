49d156d05d848322f1b70bba0ec2d1a4d1694d18
varma <phani5797@gmail.com>
2019-09-17 22:02:03 -0500

    meet doctor page changes

--- src/app/meet-doctor/meet-doctor.component.ts
+++ src/app/meet-doctor/meet-doctor.component.ts
@@ -6,6 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./meet-doctor.component.css']
 })
 export class MeetDoctorComponent implements OnInit {
+  programs = [{
+     title : 'Quality Achievement Award',
+     path: '../../assets/icon28.png'
+   },
+   {
+     title : 'Top Hospitals in the Nation',
+     path: '../../assets/icon30.png'
+   },
+   {
+     title : 'Top Performer of The Year',
+     path: '../../assets/icon18.png'
+   },
+   {
+     title : 'Quality & Accreditation',
+     path: '../../assets/icon31.png'
+   }];
 
   constructor() { }
 
