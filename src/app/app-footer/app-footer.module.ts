2965570002d0d2fe6a7ec7404de5a6abe2fe0454
varma <phani5797@gmail.com>
2019-09-22 13:34:30 -0500

    console error fix

--- src/app/app-footer/app-footer.module.ts
+++ src/app/app-footer/app-footer.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppFooterComponent } from './app-footer.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, RouterModule
   ],
   exports: [
     AppFooterComponent
